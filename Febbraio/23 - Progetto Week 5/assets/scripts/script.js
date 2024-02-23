5a107fbc3df9879e73d64ceba476a56991d598cc
rdpic <riccardodelpiccolo1@gmail.com>
2024-02-23 17:17:52 +0100

    Aggiornato al 23 Febbraio - Progetto Week 5

---
+++ Febbraio/23 - Progetto Week 5/assets/scripts/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const checkScrollAndUpdateStyles = () => {
+        const heroSection = document.querySelector('.hero');
+        const heroSectionBottom = heroSection.offsetHeight + heroSection.offsetTop;
+        const header = document.querySelector('.site-header');
+        const getStartedButton = document.querySelector('.main-btn');
+
+        if (window.scrollY > heroSectionBottom) {
+            header.style.backgroundColor = 'white';
+            header.style.transition = 'background-color 0.5s ease';
+            getStartedButton.style.backgroundColor = 'green';
+            getStartedButton.style.transition = 'background-color 0.5s ease';
+        } else {
+            header.style.backgroundColor = '';
+            getStartedButton.style.backgroundColor = '';
+        }
+    }
+
+    window.addEventListener('scroll', checkScrollAndUpdateStyles);
+
+    const toggleRandomM = () => {
+        const ms = document.querySelectorAll('.hero-m-container .m');
+        const randomIndex = Math.floor(Math.random() * ms.length);
+        const randomM = ms[randomIndex];
+
+        randomM.classList.toggle('hidden');
+
+        const randomTime = Math.random() * 1000 + 300;
+        setTimeout(toggleRandomM, randomTime);
+    }
+
+    toggleRandomM();
+})
